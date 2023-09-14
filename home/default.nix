fb4d74884427d22da5a867893f4ec928642692a7
Hadyn Youens <hadyn.youens@educationperfect.com>
2023-09-15 11:22:53 +1200

    Update p10k config

--- home/default.nix
+++ home/default.nix
@@ -158,7 +158,7 @@ in
       {
         name = "powerlevel10k-config";
         src = lib.cleanSource ./config/p10k;
-        file = "p10k-classic.zsh";
+        file = "p10k-lean.zsh";
       }
     ];
 
