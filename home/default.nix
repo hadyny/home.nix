666ad016712a8d5395555dc47660c49f1dcb082a
Hadyn Youens <hadyn.youens@educationperfect.com>
2023-09-27 16:24:46 +1300

    Start nvm automatically

--- home/default.nix
+++ home/default.nix
@@ -145,6 +145,10 @@ in
       zstyle ':bracketed-paste-magic' active-widgets '.self-*'
       autoload -U add-zsh-hook
 
+      export NVM_DIR="$([ -z "''${XDG_CONFIG_HOME-}" ] && printf %s "''${HOME}/.nvm" || printf %s "''${XDG_CONFIG_HOME}/nvm")"
+      [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+      autoload -U add-zsh-hook
       load-nvmrc() {
         local nvmrc_path
         nvmrc_path="$(nvm_find_nvmrc)"
