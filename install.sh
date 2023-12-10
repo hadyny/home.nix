6202b1068156f9574f1fa759dd10c5a894f95ab8
Hadyn Youens <hadyn@youens.nz>
2023-12-11 10:10:18 +1300

    LazyVim the nix way

--- install.sh
+++ install.sh
@@ -122,26 +122,6 @@ clone_repository() {
   cd - >/dev/null
 }
 
-clone_lazyvim() {
-  echo
-  local repository="LazyVim/starter"
-  local clone_target="${HOME}/.config/nvim"
-  header "Setting up LazyVim from github.com:${repository}..."
-
-  if [[ ! $( cat "${clone_target}/.git/config" | grep "github.com" | grep "${repository}" ) ]]; then
-    if [ -d "${clone_target}" ]; then
-      warn "Looks like '${clone_target}' exists and it is not what we want. Backing up as '${clone_target}.backup-before-clone'..."
-      mv "${clone_target}" "${clone_target}.backup-before-clone"
-    fi
-    warn "Cloning 'github.com:${repository}' into '${clone_target}'..."
-    git clone "https://github.com/${repository}.git" "${clone_target}"
-  fi
-
-  info "'${clone_target}' is sourced from github.com:'${repository}'."
-  cd "${clone_target}"
-  cd - >/dev/null
-}
-
 set_up_secrets() {
   echo
   header "Setting up secrets"
@@ -206,6 +186,5 @@ install_nix_darwin
 clone_repository
 set_up_secrets
 darwin_build
-clone_lazyvim
 
 } # Prevent script running if partially downloaded
