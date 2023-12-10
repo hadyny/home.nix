c94dc124be3d0618294e676bac4908af52a4ddfc
Hadyn Youens <hadyn@youens.nz>
2023-12-11 09:37:45 +1300

    Command line tweaks

--- home/default.nix
+++ home/default.nix
@@ -80,9 +80,12 @@ in
     eza = {
       enable = true;
       enableAliases = true;
+      git = true;
+      icons = true;
       extraOptions = [
         "--group-directories-first"
         "--header"
+        "--long"
       ];
     };
 
@@ -101,6 +104,20 @@ in
       enable = true;
       enableZshIntegration = true;
       enableBashIntegration = true;
+      colors = {
+        "bg+" = "#313244"; 
+        "bg" = "#1e1e2e";
+        "spinner" = "#f5e0dc";
+        "hl" = "#f38ba8";
+        "fg" = "#cdd6f4";
+        "header" = "#f38ba8";
+        "info" = "#cba6f7";
+        "pointer" = "#f5e0dc";
+        "marker" = "#f5e0dc";
+        "fg+" = "#cdd6f4";
+        "prompt" = "#cba6f7";
+        "hl+" = "#f38ba8";
+      };
     };
 
     lf.enable = true;
