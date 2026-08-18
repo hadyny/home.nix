7174d355e1a2a8fdf46fb6a0da0710b81e795365
Hadyn Youens <hadyn.youens@educationperfect.com>
2026-08-19 09:52:02 +1200

    rename canada profile name

--- modules/darwin/work/aws.nix
+++ modules/darwin/work/aws.nix
@@ -73,7 +73,7 @@
         region = "ap-southeast-2";
       };
 
-      canada = {
+      live-ca-central-1 = {
         sso_start_url = "https://educationperfect.awsapps.com/start";
         sso_account_id = "176806616083";
         sso_role_name = "admin";
